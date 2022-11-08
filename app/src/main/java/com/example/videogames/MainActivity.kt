fae5f06cbf5b9ab58ac52b75c98a0672f665135a
Rubén Sánchez <rsanchez@ucc.mx>
2022-11-07 21:02:25 -0600

    Formulario registro

--- app/src/main/java/com/example/videogames/MainActivity.kt
+++ app/src/main/java/com/example/videogames/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.videogames
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fabNuevo : FloatingActionButton = findViewById(R.id.fabNuevo)
+
+        fabNuevo.setOnClickListener {
+            nuevoVideoGame()
+        }
+    }
+
+    private fun nuevoVideoGame() {
+        val intent : Intent = Intent(this, VideoGameActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/videogames/VideoGameActivity.kt
+++ app/src/main/java/com/example/videogames/VideoGameActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import com.android.volley.toolbox.Volley
 
 class VideoGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,44 @@ class VideoGameActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.opc_guardar){
+            guardar()
+        }
+
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun guardar() {
+        val etTitulo : EditText = findViewById(R.id.etTitulo)
+        val etPrecio : EditText = findViewById(R.id.etPrecio)
+
+        val cbXbox : CheckBox = findViewById(R.id.cbXbox)
+        val cbPlaystation : CheckBox = findViewById(R.id.cbPlaystation)
+        val cbNintendo : CheckBox = findViewById(R.id.cbNintendo)
+        val cbPc : CheckBox = findViewById(R.id.cbPc)
+
+        val rbNuevo : RadioButton = findViewById(R.id.rbNuevo)
+        val rbUsado : RadioButton = findViewById(R.id.rbUsado)
+        ////////////////////////////////////////////////////////////////////////////////
+
+        val titulo : String = etTitulo.text.toString()
+        val precio : String = etPrecio.text.toString()
+
+        val xbox : Int          = if (cbXbox.isChecked) 1 else 0
+        val playstation : Int   = if (cbPlaystation.isChecked) 1 else 0
+        val nintendo : Int      = if (cbNintendo.isChecked) 1 else 0
+        val pc : Int            = if (cbPc.isChecked) 1 else 0
+
+        val estado : String     = if (rbNuevo.isChecked) "N" else "U"
+
+        enviarDatos()
+    }
+
+    private fun enviarDatos() {
+        // cola de peticiones
+        val queue = Volley.newRequestQueue(this)
+
+
     }
 }
